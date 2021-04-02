147a0a54e69182679d33bc2cdfb7127a7aed2751
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-04-02 14:42:59 -0600

    set up the form handlers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
-import About from './components/About';
-import Nav from './components/Nav';
-import Gallery from './components/Gallery'
+import React, { useState } from "react";
+import About from "./components/About";
+import Nav from "./components/Nav";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
     {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
     },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
@@ -19,15 +24,14 @@ function App() {
   return (
     <div>
       <Nav
-      categories={categories}
-      setCurrentCategory={setCurrentCategory}
-      currentCategory={currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
       ></Nav>
       <main>
-    
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
-        
       </main>
     </div>
   );
