643bceb9e57b9de511e13453f9673f43d4ddbfdf
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2021-04-02 15:51:42 -0600

    refactoring

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -14,4 +14,14 @@ describe("Contact is rendering", () => {
     const { asFragment } = render(<Contact />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it("renders", () => {
+    const { getByTestId } = render(<Contact />);
+    expect(getByTestId("h1tag")).toHaveTextContent("Contact me");
+  });
+
+  it("renders", () => {
+    const { getByTestId } = render(<Contact />);
+    expect(getByTestId("button")).toHaveTextContent("Submit");
+  });
 });
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,19 +11,22 @@ function ContactForm() {
   const [errorMessage, setErrorMessage] = useState("");
 
   function handleChange(e) {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      if (!isValid) {
-        setErrorMessage("Your email is invalid");
+    const handleChange = (e) => {
+      if (e.target.name === "email") {
+        const isValid = validateEmail(e.target.value);
+        if (!isValid) {
+          setErrorMessage("Your email is invalid.");
+        } else {
+          setErrorMessage("");
+        }
       } else {
         if (!e.target.value.length) {
-          setErrorMessage(`${e.target.name} is required`);
+          setErrorMessage(`${e.target.name} is required.`);
         } else {
           setErrorMessage("");
         }
       }
-    }
+    };
 
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
@@ -32,12 +35,15 @@ function ContactForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(formState);
+    if (!errorMessage) {
+        setFormState({ [e.target.name]: e.target.value });
+        console.log('Form', formState);
+    }
   }
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -67,11 +73,13 @@ function ContactForm() {
           />
         </div>
         {errorMessage && (
-            <div>
-                <p className="error-test">{errorMessage}</p>
-            </div>
+          <div>
+            <p className="error-test">{errorMessage}</p>
+          </div>
         )}
-        <button type="submit">Submit</button>
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
